e7e2b6708a7138518bb2e1992e2d1fc4147af9cd
Noam Shalom <noam.shalom@post.runi.ac.il>
2026-01-02 19:26:55 +0200

    HW7 solution

--- IntToBin.java
+++ IntToBin.java
@@ -8,9 +8,8 @@ public class IntToBin {
 
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
     public static String toBinary(int x) {
-        if(x == 0) return "0";
-        if (x == 1) return "1";            
-        
-        return toBinary(x/2) + (x);
-    }    
+       if (x == 0 || x == 1)
+       return "" + x;
+       return toBinary(x / 2) + (x % 2);
+    }
  }

5aa2201b9c4f544276cc0db7eb523799fa434399
Noam Shalom <noam.shalom@post.runi.ac.il>
2026-01-02 19:13:03 +0200

    HW7 solution

--- Koch.java
+++ Koch.java
@@ -17,26 +17,65 @@ public class Koch {
 		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
 		*/
 
-		/*
+		
 		// Tests the snowflake function:
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
+        /*
 		snowFlake(Integer.parseInt(args[0]));
-		*/
+        */
+		
 	}
 
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
-	}
+        if (n == 0) {
+        StdDraw.line(x1, y1, x2, y2);
+        return;
+        }
+
+        double dx = (x2 - x1) / 3.0;
+        double dy = (y2 - y1) / 3.0;
+
+        double xA = x1;
+        double yA = y1;
+
+        double xB = x1 + dx;
+        double yB = y1 + dy;
+
+        double xD = x1 + 2 * dx;
+        double yD = y1 + 2 * dy;
+
+        double xE = x2;
+        double yE = y2;
+
+        double xC = (Math.sqrt(3) / 6.0) * (y1 - y2) + (x1 + x2) / 2.0;
+        double yC = (Math.sqrt(3) / 6.0) * (x2 - x1) + (y1 + y2) / 2.0;
+
+	    curve(n - 1, xA, yA, xB, yB);
+        curve(n - 1, xB, yB, xC, yC);
+        curve(n - 1, xC, yC, xD, yD);
+        curve(n - 1, xD, yD, xE, yE);
+}
+
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
 	public static void snowFlake(int n) {
 		// A little tweak that makes the drawing look better
-		StdDraw.setYscale(0, 1.1);
-		StdDraw.setXscale(0, 1.1);
-		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+	    StdDraw.setYscale(0, 1.1);
+        StdDraw.setXscale(0, 1.1);
+
+        double x1 = 0.2, y1 = 0.8;
+        double x2 = 0.8, y2 = 0.8;
+
+        double side = x2 - x1;
+        double h = (Math.sqrt(3) / 2.0) * side;
+
+        double x3 = 0.5;
+        double y3 = y1 - h;
+
+        curve(n, x1, y1, x2, y2); 
+        curve(n, x2, y2, x3, y3); 
+        curve(n, x3, y3, x1, y1); 
 	}
 }
--- Sierpinski.java
+++ Sierpinski.java
@@ -7,12 +7,44 @@ public class Sierpinski {
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
-	}
+        double x1 = 0.1, y1 = 0.1;  
+        double x2 = 0.9, y2 = 0.1;   // ימין-למטה
+
+        double side = x2 - x1;
+        double h = (Math.sqrt(3) / 2) * side;
+
+        double x3 = 0.5;             
+        double y3 = y1 + h;         
+
+        StdDraw.line(x1, y1, x2, y2);
+        StdDraw.line(x2, y2, x3, y3);
+        StdDraw.line(x3, y3, x1, y1);
+
+        if (n > 0) {
+            sierpinski(n - 1, x1, x2, x3, y1, y2, y3);
+	    }
+    }
 	
 	// Does the actual drawing, recursively.
-	private static void sierpinski(int n, double x1, double x2, double x3,
-		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+	private static void sierpinski(int n, double x1, double x2, double x3, double y1, double y2, double y3) {
+	 if (n == 0) {
+            return; 
+        }
+        double mx12 = (x1 + x2) / 2.0;
+        double my12 = (y1 + y2) / 2.0;
+
+        double mx23 = (x2 + x3) / 2.0;
+        double my23 = (y2 + y3) / 2.0;
+
+        double mx31 = (x3 + x1) / 2.0;
+        double my31 = (y3 + y1) / 2.0;
+
+        StdDraw.line(mx12, my12, mx23, my23);
+        StdDraw.line(mx23, my23, mx31, my31);
+        StdDraw.line(mx31, my31, mx12, my12);
+
+        sierpinski(n - 1, x1, mx12, mx31, y1, my12, my31);
+        sierpinski(n - 1, mx12, x2, mx23, my12, y2, my23);
+        sierpinski(n - 1, mx31, mx23, x3, my31, my23, y3);
 	}
 }
